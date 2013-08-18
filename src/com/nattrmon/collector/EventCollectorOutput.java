9fdc5f331259ccde4cc0830efdd966a5be15c388
nuno <nuno@aguiar.name>
2013-08-19 00:47:37 +0100

    Using a better implementation with java.util.concurrent.*

--- src/com/nattrmon/collector/EventCollectorOutput.java
+++ src/com/nattrmon/collector/EventCollectorOutput.java
@@ -18,9 +18,17 @@
  */
 package com.nattrmon.collector;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
-import com.nattrmon.collector.TimerCollectorOutput.TimerOutputTask.OutputThread;
+import com.nattrmon.collector.TimerCollectorOutput.OutputThread;
+import com.nattrmon.collector.TimerCollectorOutput.OutputsThread;
 import com.nattrmon.config.Config;
 import com.nattrmon.core.OutputFormat;
 import com.nattrmon.output.Output.OutputType;
@@ -41,23 +49,27 @@ public class EventCollectorOutput implements CollectorOutput {
 	protected HashMap<OutputFormat, OutputThread> threads = new HashMap<OutputFormat, OutputThread>();
 	protected int currentCount = 0;
 	protected boolean hasChanged = false;
+	protected static ScheduledExecutorService fillOutputs;
+	protected static ExecutorService execOutputs;
+	protected static ArrayList<OutputThread> outputs;
+	protected boolean waitForOutput = true;
 	
-	synchronized void incCount() {
-		currentCount++;
-	}
-	
-	synchronized void decCount() {
-		currentCount--;
-	}
-	
-	synchronized int getCount() {
-		return currentCount;
-	}
-	
-	synchronized void zeroCount() {
-		currentCount = 0;
-	}
-	
+//	synchronized void incCount() {
+//		currentCount++;
+//	}
+//	
+//	synchronized void decCount() {
+//		currentCount--;
+//	}
+//	
+//	synchronized int getCount() {
+//		return currentCount;
+//	}
+//	
+//	synchronized void zeroCount() {
+//		currentCount = 0;
+//	}
+//	
 	synchronized boolean isChanged() {
 		return hasChanged;
 	}
@@ -66,108 +78,109 @@ public class EventCollectorOutput implements CollectorOutput {
 		hasChanged = c;
 	}
 	
-	class OutputThread extends Thread {
-		Object lock = new Object();
-		OutputFormat output;
-		
-		public void run() {
-			while (true) {
-				synchronized (lock) {
-					try {
-						try {
-							lock.wait();
-						} catch (InterruptedException e) {
-						}
-
-						// call a second time if it's the first time and show
-						// header is active
-						if (output.isFirstTime() && output.isShowHeader())
-							output.output();
-
-						// normal call
-						output.preOutput();
-						if (!(output.isAttributeValuesEqualToLastRun())) {
-							output.processOutput();
-							output.posOutput();
-							setChanged(true);
-						}
-					} catch (Exception e) {
-						conf.lOG(OutputType.ERROR, "Exception", e);
-					} finally {
-						decCount();
-					}
-				}
-			}
-		}
-		
-		public OutputThread(OutputFormat o) {
-			super();
-			
-			output = o;
-			setPriority(Thread.MAX_PRIORITY - 1);
-		}
-	}
-	
-	@Override
-	public void run() {
-
-		while(true) {	
-			
-			// Set threads if needed
-			zeroCount();
-			for(OutputFormat out :conf.getOutputformats()) {
-				incCount();
-				if (threads.containsKey(out)) {
-					// Nothing.. for now
-				} else {
-					OutputThread ot = new OutputThread(out);
-					ot.setPriority(Thread.MAX_PRIORITY - 1);
-					ot.setDaemon(true);
-					ot.start();
-					threads.put(out, ot);
-				}
-			}
-			
-			// Start threads
-			for(OutputFormat t : threads.keySet()) {
-				threads.get(t).setPriority(Thread.MAX_PRIORITY - 1);
-				threads.get(t).interrupt();
-			}
-			
-			// Wait for threads
-			while(getCount() > 0) {
-				try {
-					Thread.sleep(50);
-				} catch (InterruptedException e) {
-				}
-			}
-			
-			// Put to sleep threads
-			for(OutputFormat t : threads.keySet()) {
-				threads.get(t).setPriority(Thread.MIN_PRIORITY);
-			}
-			
-			if (isChanged()) {
-				setChanged(false);
-			}
-			
-			OutputFormat.cleanAttributeValues();
-			increaseGeneralCounter();
-			
-			if (internalCount > 0) {
-				if (conf.getCurrentCounter() >= internalCount) {
-					System.exit(0);
-					conf.lOG(OutputType.DEBUG, "Done");
-				}
-			}
-			
-			try {
-				Thread.sleep(timeInterval);
-			} catch (InterruptedException e) {
-			}
-		}
-		
-	}
+//	class OutputThread extends Thread {
+//		Object lock = new Object();
+//		OutputFormat output;
+//		
+//		public void run() {
+//			while (true) {
+//				synchronized (lock) {
+//					try {
+//						try {
+//							lock.wait();
+//						} catch (InterruptedException e) {
+//						}
+//
+//						long counter = conf.getCurrentCounter();
+//						// call a second time if it's the first time and show
+//						// header is active
+//						if (output.isFirstTime() && output.isShowHeader())
+//							output.output(counter);
+//
+//						// normal call
+//						output.preOutput(counter);
+//						if (!(output.isAttributeValuesEqualToLastRun())) {
+//							output.processOutput(counter);
+//							output.posOutput(counter);
+//							setChanged(true);
+//						}
+//					} catch (Exception e) {
+//						conf.lOG(OutputType.ERROR, "Exception", e);
+//					} finally {
+//						decCount();
+//					}
+//				}
+//			}
+//		}
+//		
+//		public OutputThread(OutputFormat o) {
+//			super();
+//			
+//			output = o;
+//			setPriority(Thread.MAX_PRIORITY - 1);
+//		}
+//	}
+//	
+//	@Override
+//	public void run() {
+//
+//		while(true) {	
+//			
+//			// Set threads if needed
+//			zeroCount();
+//			for(OutputFormat out :conf.getOutputformats()) {
+//				incCount();
+//				if (threads.containsKey(out)) {
+//					// Nothing.. for now
+//				} else {
+//					OutputThread ot = new OutputThread(out);
+//					ot.setPriority(Thread.MAX_PRIORITY - 1);
+//					ot.setDaemon(true);
+//					ot.start();
+//					threads.put(out, ot);
+//				}
+//			}
+//			
+//			// Start threads
+//			for(OutputFormat t : threads.keySet()) {
+//				threads.get(t).setPriority(Thread.MAX_PRIORITY - 1);
+//				threads.get(t).interrupt();
+//			}
+//			
+//			// Wait for threads
+//			while(getCount() > 0) {
+//				try {
+//					Thread.sleep(50);
+//				} catch (InterruptedException e) {
+//				}
+//			}
+//			
+//			// Put to sleep threads
+//			for(OutputFormat t : threads.keySet()) {
+//				threads.get(t).setPriority(Thread.MIN_PRIORITY);
+//			}
+//			
+//			if (isChanged()) {
+//				setChanged(false);
+//			}
+//			
+//			OutputFormat.cleanAttributeValues();
+//			increaseGeneralCounter();
+//			
+//			if (internalCount > 0) {
+//				if (conf.getCurrentCounter() >= internalCount) {
+//					System.exit(0);
+//					conf.lOG(OutputType.DEBUG, "Done");
+//				}
+//			}
+//			
+//			try {
+//				Thread.sleep(timeInterval);
+//			} catch (InterruptedException e) {
+//			}
+//		}
+//		
+//	}
 	
 	/**
 	 * Increase the general counter
@@ -195,12 +208,116 @@ public class EventCollectorOutput implements CollectorOutput {
 		
 		Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
 		
+		String settingsParams = conf.getParams();
+		if (settingsParams != null) {
+			String pms[] = settingsParams.split(";");
+			String tmp = "";
+			String prop[];
+	
+			for (String p : pms) {
+				tmp = p.trim();
+				prop = tmp.split("=");
+				if (prop.length == 2) {
+					if (prop[0].equalsIgnoreCase("wait"))
+						if (prop[1].equalsIgnoreCase("y"))
+							waitForOutput = true;
+//					if (prop[0].equalsIgnoreCase("timeout"))
+//						try { timeout = Long.valueOf(prop[1]); } catch (Exception e) {} 
+				}
+			}
+		}
+		
+		fillOutputs = Executors.newScheduledThreadPool(1);
+		
 		run();
+	}
+	
+	public void run() {
+		ScheduledFuture<?> f;
+		if (waitForOutput) {
+			f = fillOutputs.scheduleWithFixedDelay(new OutputsThread(), 0, timeInterval, TimeUnit.MILLISECONDS);
+		} else {
+			f = fillOutputs.scheduleAtFixedRate(new OutputsThread(), timeInterval, 0, TimeUnit.MILLISECONDS);
+		}
 	}
 	
 	@Override
 	public Config getConfig() {
 		return conf;
+	}
+	
+	class OutputsThread implements Runnable {
+
+		@Override
+		public void run() {
+			if (execOutputs == null)
+				execOutputs = Executors.newFixedThreadPool(conf.getOutputformats().size());
+			
+			if (outputs == null) {
+				outputs = new ArrayList<OutputThread>();
+				for(OutputFormat out : conf.getOutputformats()) {
+					outputs.add(new OutputThread(out));
+				}
+			}
+			
+			try {
+				execOutputs.invokeAll(outputs);
+			} catch (InterruptedException e) {
+			}
+			
+			if (isChanged()) {
+				setChanged(false);
+			}
+			increaseGeneralCounter();
+		}
+		
+	}
+	
+	class OutputThread implements Callable<Boolean> {
+		//Object lock = new Object();
+		OutputFormat output;
+		
+		public Boolean call() {
+			//while (true) {
+				//synchronized (lock) {
+					try {
+//						try {
+//							startSignal.lockInterruptibly();
+//						} catch (InterruptedException e1) {
+//						}
+						
+						long counter = conf.getCurrentCounter();
+						
+						// call a second time if it's the first time and show header is active
+						if (output.isFirstTime() && output.isShowHeader()) {
+							output.output(-1); 
+						}
+						
+						// normal call
+						output.preOutput(counter);
+						if (!(output.isAttributeValuesEqualToLastRun())) {
+							output.processOutput(counter);
+							output.posOutput(counter);
+							setChanged(true);
+						}
+					} catch (Exception e) {
+						conf.lOG(OutputType.DEBUG, "Exception", e);
+					} finally {
+						//decCount();
+					}
+					
+					return true;
+				//}
+			//}
+		}
+		
+		public OutputThread(OutputFormat o) {
+			output = o;
+//			setPriority(Thread.MAX_PRIORITY - 1);
+//			synchronized (threads) {
+//				threads.put(o, this);
+//			}
+		}
 	}
 
 }
